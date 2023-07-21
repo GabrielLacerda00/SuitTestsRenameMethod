c079d28df953c8bfb83436d583ec4dc4efd1e5eb
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-07-21 13:33:46 -0300

    Before teste refactoring: rename method

---
+++ OneRenameForTwoCallerFromSuperClasse/TesteRefactoring/calculadora.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Method;
+
+public class calculadora extends calc{
+  public static void main(String[] args) {
+    soma();
+    soma();
+  }
+
+  public static double soma() {
+    return 1.0 + 2.0;
+  }
+
+}
