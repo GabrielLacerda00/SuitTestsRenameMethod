b66d14dec837d9193e24b11b435fd8a7c04775fc
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-07-23 20:02:16 -0300

    code afefter refactoring rename method

--- OneRenameForTwoCallersInClassRefact/src/calculadoraCientifica/calc.java
+++ OneRenameForTwoCallersInClassRefact/src/calculadoraCientifica/calc.java
@@ -1,11 +1,11 @@
 public class calc {
   public static void main(String[] args) {
     calc cal = new calc();
-    cal.soma();
-    cal.soma();
+    cal.sum();
+    cal.sum();
   }
   
-  public double soma() {
+  public double sum() {
       return 1.0 + 2.0;
   }
 }
