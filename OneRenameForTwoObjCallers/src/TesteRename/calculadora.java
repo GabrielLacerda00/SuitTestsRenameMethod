1c54950e1daa9ec4e8ecd4c55b0ae8c90b9dde48
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-07-22 23:19:39 -0300

    code after rename method for two obj callers2.0

--- OneRenameForTwoObjCallers/src/TesteRename/calculadora.java
+++ OneRenameForTwoObjCallers/src/TesteRename/calculadora.java
@@ -3,11 +3,11 @@ import java.lang.reflect.Method;
 public class calculadora extends calc{
   public static void main(String[] args) {
     calc cal = new calc();
-    cal.soma();
-    cal.soma();
+    cal.sum();
+    cal.sum();
   }
 
-  public double soma() {
+  public double sum() {
     return 1.0 + 2.0;
   }
 
