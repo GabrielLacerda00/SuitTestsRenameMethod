59046c6532e950f4a951f88ab2829a79f1989ff2
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-07-25 22:40:46 -0300

    code after rename method: rename in one class and in superClass

--- RenameInClassAndSupeClass/src/calculadora.java
+++ RenameInClassAndSupeClass/src/calculadora.java
@@ -3,10 +3,10 @@ import java.lang.reflect.Method;
 public class calculadora extends calc{
   public static void main(String[] args) {
     calc cal = new calc();
-    cal.soma();
+    cal.sum();
   }
 
-  public double soma() {
+  public double sum() {
     return 1.0 + 2.0;
   }
 
--- RenameInClassAndSupeClass/src/calculadoraCientifica/calc.java
+++ RenameInClassAndSupeClass/src/calculadoraCientifica/calc.java
@@ -1,10 +1,10 @@
 public class calc {
   public static void main(String[] args) {
     calc cal = new calc();
-    cal.soma();
+    cal.sum();
   }
   
-  public double soma() {
+  public double sum() {
       return 1.0 + 2.0;
   }
 }
