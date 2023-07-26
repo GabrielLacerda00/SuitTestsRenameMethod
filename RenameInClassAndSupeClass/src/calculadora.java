b0f73242086dfde70001f2731ffcb0fc4eaa0354
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-07-25 23:07:03 -0300

    reset

--- RenameInClassAndSupeClass/src/calculadora.java
+++ RenameInClassAndSupeClass/src/calculadora.java
@@ -3,10 +3,10 @@ import java.lang.reflect.Method;
 public class calculadora extends calc{
   public static void main(String[] args) {
     calc cal = new calc();
-    cal.sum();
+    cal.soma();
   }
 
-  public double sum() {
+  public double soma() {
     return 1.0 + 2.0;
   }
 
--- RenameInClassAndSupeClass/src/calculadoraCientifica/calc.java
+++ RenameInClassAndSupeClass/src/calculadoraCientifica/calc.java
@@ -1,10 +1,10 @@
 public class calc {
   public static void main(String[] args) {
     calc cal = new calc();
-    cal.sum();
+    cal.soma();
   }
   
-  public double sum() {
+  public double soma() {
       return 1.0 + 2.0;
   }
 }
